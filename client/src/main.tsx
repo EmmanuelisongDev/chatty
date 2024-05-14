a4c8eaa5e9f2ad4db9cc7f2e491893bbe296b6f1
Emmanuel Isong <emmisongdev@gmail.com>
2024-05-14 17:30:45 +0100

    Added Home Page

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import '@fontsource/inter'
+import { ChakraProvider,extendTheme } from '@chakra-ui/react'
+
+
+    const theme =  extendTheme({
+      colors:{
+        brand:{
+          50:"#FF6E40",
+          100:"#1E3D59",
+          200:"#F5F0E1"
+        },
+      },
+      fonts: {
+        heading: `'Inter', sans-serif`,
+        body: `'Inter', sans-serif`,
+      },
+    })
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <ChakraProvider theme={theme}>
     <App />
+    </ChakraProvider>
   </React.StrictMode>,
 )
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,14 +1,48 @@
+import {  Button,
+  Center,
+  Container,
+  Heading,
+  Box,
+  Text,} from "@chakra-ui/react"
 
 export default function HomePage() {
   return (
-    <div>
-    
-        <form action="">
-          
-          <input type="text" placeholder="Please Nick name" name="" id="" required />
-          <button type='submit'>Go to Chatty</button>
-        </form>
+   <Container>
+    <Center mt={4}>
+      <Heading
+       color='brand.100'
+      fontWeight='extrabold'
+       
+       >Chatty</Heading>
+      <Heading
+       fontWeight='extrabold'
+      color='brand.50
+      '>.</Heading>
+    </Center>
 
-    </div>
+
+    <Center mt={10}>
+      <Text
+       color='brand.100'
+       fontWeight='extrabold'
+       >Just Chat</Text>
+      <Text 
+       fontWeight='extrabold'
+      color='brand.50
+      font
+      '>!</Text>
+    </Center>
+
+
+
+
+
+
+   </Container>
+
+
+        
+    
+   
   )
 }
